fbb836ace036523e87e82f59fe523204e7aed10a
Matteo Novelli <mnovelli@openfinance.es>
2020-01-03 14:10:11 +0100

    Refactor obstacle and removed dist

--- src/obstacle.js
+++ src/obstacle.js
@@ -1,10 +1,6 @@
 import { GameObject } from './gameObject';
 
 class Obstacle extends GameObject {
-  constructor (elementId, x, y) {
-    super(elementId);
-    this.position = { x, y };
-  }
 }
 
 export { Obstacle };
